4e60ab26f84279181836e9638a724709ea5176d0
Cam Manderson <cam@alpaca.travel>
2020-06-12 15:55:36 +1000

    Renames and makes more generic to support parsing expressions (such as fexp) or using your own method pattern

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-const { parse } = require("@alpaca-travel/fexp-js");
 const _isEqual = require("lodash.isequal");
+const _clone = require("lodash.clone");
 const assert = require("assert");
-const objectActions = require("./object-actions");
 
-const supportedRuleTypes = Object.keys(objectActions);
+const remove = (args) => {
+  const clone = _clone(args[1] || args.context.vars.arguments[0]);
+  const props = [...args];
+  delete clone[props[0]];
+  return clone;
+};
 
 /**
  * Take a set of defined rules, and validate/optimise
  *
  * rules: [
  *   {
- *     match: _fexp-js-expression_
- *     actions: [
- *       { type: 'remove', args: [..] }
- *     ]
+ *     match: <fexp-js-expression to return true/false>
+ *     perform: <fexp-js-expresion to modify>
  *   }
  * ]
  **/
-const rulesParse = (rules, { warn, lang = stdLang }) =>
-  rules
+const rulesParse = (rules, { warn, lang, parse }) => {
+  // Fexpjs
+  const mutatorLang = {
+    remove,
+  };
+  const langImplementation = Object.assign(lang || {}, mutatorLang);
+
+  return rules
     .map((r) => {
       try {
-        assert(Array.isArray(r.actions));
-        assert(r.actions.length, "Should supply actions for this");
-        assert(r.match, "Should provide a match statement");
-        r.actions.forEach((rule) => {
-          assert(rule.type, "Should define a supported type");
-          assert(
-            supportedRuleTypes.indexOf(rule.type) > -1,
-            `Unrecognised rule action type ${
-              rule.type
-            }. Allowed values are: ${supportedRuleTypes.join(",")}`
-          );
-          assert(Array.isArray(rule.args), `Rule ${rule.type} is missing args`);
-        });
+        assert(
+          Array.isArray(r.perform) || r.perform,
+          "Should supply perform for this"
+        );
+        assert(
+          Array.isArray(r.match) ||
+            ["function", "boolean"].indexOf(typeof r.match) > -1,
+          "Should provide a match statement"
+        );
       } catch (e) {
         if (warn === true) {
           console.warn(e);
@@ -42,10 +46,28 @@ const rulesParse = (rules, { warn, lang = stdLang }) =>
         throw e;
       }
       return Object.assign({}, r, {
-        parsed: parse(r.match, lang),
+        matcher: (() => {
+          if (typeof r.match === "boolean") {
+            return r.match;
+          }
+          if (Array.isArray(r.match)) {
+            return parse(r.match, langImplementation);
+          }
+          return r.match;
+        })(),
+        performer: (() => {
+          if (typeof r.perform === "function") {
+            return r.perform;
+          }
+          if (Array.isArray(r.perform)) {
+            return parse(r.perform, langImplementation);
+          }
+          return r.perform;
+        })(),
       });
     })
     .filter((rule) => rule);
+};
 
 /**
  * Process an assignment
@@ -64,6 +86,7 @@ const assign = (options) => {
     return rulesParse(options.rules, {
       warn: options.warn === true,
       lang: options.lang,
+      parse: options.parse,
     });
   })();
 
@@ -98,22 +121,33 @@ const assignOnce = (before, apply, options = {}) => {
     const next = rulesParsed
       .filter((rule) => {
         // If the match consition is matched
-        if (rule.parsed(data)) {
-          // Indicate we want to process the actions
-          return true;
-        }
-        // Ignore this rule
-        return false;
+        const isMatch = (() => {
+          // We are appling
+          if (typeof rule.matcher === "function") {
+            if (rule.matcher(data.before, data.next)) {
+              // Indicate we want to process the actions
+              return true;
+            }
+          }
+          // We have a boolean
+          if (typeof rule.matcher === "boolean") {
+            return rule.matcher;
+          }
+          return false;
+        })();
+        return isMatch;
       })
       .reduce((trans, rule) => {
         // Apply the transition rules
-        const next = rule.actions.reduce(
-          (next, action) => objectActions[action.type](next)(...action.args),
-          trans
-        );
+        const next = (() => {
+          if (typeof rule.performer === "function") {
+            return rule.performer(trans);
+          }
+          return rule.performer;
+        })();
 
         // Capture mistakes where the value does not transition
-        if (_isEqual(trans, next)) {
+        if (_isEqual(next, trans)) {
           console.warn("Rule did not transition and is removed", rule);
           rulesParsed = rulesParsed.filter((f) => f !== rule);
         }
@@ -133,7 +167,51 @@ const assignOnce = (before, apply, options = {}) => {
   return transitioning;
 };
 
+// reducer-assign, match, result, match, result, action, action
+
+const reducerAssign = (args) => {
+  const iterator = args[Symbol.iterator]();
+
+  const pair = function* () {
+    while (true) {
+      const arg0 = iterator.next();
+      const arg1 = iterator.next();
+      if (!arg1.done) {
+        yield [arg0.value, arg1.value];
+      } else {
+        yield arg0.value;
+        break;
+      }
+    }
+  };
+
+  const rules = [];
+  let assignVal = {};
+
+  for (const cases of pair(args)) {
+    if (Array.isArray(cases)) {
+      const [kase, output] = cases;
+      rules.push({
+        match: kase,
+        perform: output,
+      });
+    } else {
+      assignVal = cases;
+    }
+  }
+
+  const context = args.context.vars.arguments[0];
+
+  return assignOnce(context, assignVal, { rulesParsed: rulesParse(rules, {}) });
+};
+
+const lang = {
+  "reducer-assign": reducerAssign,
+  remove,
+};
+
 module.exports = {
   assign,
   rulesParse,
+  lang,
 };
--- tests/src/index.test.js
+++ tests/src/index.test.js
@@ -1,47 +1,97 @@
-const lang = require("@alpaca-travel/fexp-js-lang");
-const { assign } = require("../../src/index");
+const { parse, langs } = require("@alpaca-travel/fexp-js");
+const stdLang = require("@alpaca-travel/fexp-js-lang");
+const { assign, lang: shimLang } = require("../../src/index");
 
 describe("State Transitions", () => {
+  test("Fexp-js reducer fn", () => {
+    const lang = langs(stdLang, shimLang);
+    const expr = [
+      "reducer-assign",
+      [
+        "fn",
+        [
+          "all",
+          // Change in category
+          ["!=", ["get", "category"], ["get", "category", ["fn-arg", 1]]],
+          // Has a subcategory, and still has a subcategory
+          ["exists", ["get", "subCategory"]],
+          ["exists", ["get", "subCategory", ["fn-arg", 1]]],
+        ],
+      ],
+      ["fn", ["remove", "subCategory"]],
+      ["fn-arg", 1],
+    ];
+
+    const fn = parse(expr, lang);
+    expect(fn({ category: "a", subCategory: "a" }, { category: "b" })).toEqual({
+      category: "b",
+    });
+  });
+
   describe("Basic category/sub-category dependencies", () => {
-    test("Scenarios", () => {
+    test("standard", () => {
+      const rules = [
+        {
+          match: (before, after) => {
+            if (
+              before.category !== after.category &&
+              before.subCategory &&
+              after.subCategory
+            ) {
+              return true;
+            }
+            return false;
+          },
+          perform: (obj) => {
+            const val = Object.assign({}, obj, { subCategory: null });
+            return val;
+          },
+        },
+      ];
+
+      const state = {
+        category: "eat",
+        subCategory: "restaurant",
+        tags: ["tag1"],
+      };
+      const changeState = { category: "stay" };
+
+      const value = assign({ rules })(state, changeState);
+      expect(value).toEqual({
+        category: "stay",
+        subCategory: null,
+        tags: ["tag1"],
+      });
+    });
+    test("Scenarios using fexp-js", () => {
       const rules = [
         // Remove a sub-category if something changes
         {
           match: [
             "all",
             // Change in category
-            ["!=", ["get", "before.category"], ["get", "next.category"]],
+            ["!=", ["get", "category"], ["get", "category", ["fn-arg", 1]]],
             // Has a subcategory, and still has a subcategory
-            ["exists", ["get", "before.subCategory"]],
-            ["exists", ["get", "next.subCategory"]],
+            ["exists", ["get", "subCategory"]],
+            ["exists", ["get", "subCategory", ["fn-arg", 1]]],
           ],
-          actions: [
-            {
-              type: "remove",
-              args: ["subCategory"],
-            },
-          ],
+          perform: ["remove", "subCategory"],
         },
         // Remove a category when you change a region
         {
           match: [
             "all",
             // Change in category
-            ["!=", ["get", "before.region"], ["get", "next.region"]],
-            ["exists", ["get", "before.category"]],
-            ["exists", ["get", "next.category"]],
+            ["!=", ["get", "region"], ["get", "region", ["fn-arg", 1]]],
+            ["exists", ["get", "category"]],
+            ["exists", ["get", "category", ["fn-arg", 1]]],
           ],
-          actions: [
-            {
-              type: "remove",
-              args: ["category"],
-            },
-          ],
+          perform: ["remove", "category"],
         },
       ];
 
       expect(
-        assign({ rules, lang })(
+        assign({ rules, lang: stdLang, parse })(
           { category: "a", subCategory: "a", region: "a" },
           { category: "a", region: "b" }
         )
@@ -50,7 +100,7 @@ describe("State Transitions", () => {
       });
 
       expect(
-        assign({ rules, lang })(
+        assign({ rules, lang: stdLang, parse })(
           { category: "a", subCategory: "a", region: "a" },
           { category: "b", region: "a" }
         )
@@ -60,7 +110,7 @@ describe("State Transitions", () => {
       });
 
       expect(
-        assign({ rules, lang })(
+        assign({ rules, lang: stdLang, parse })(
           { category: "a", subCategory: "a", region: "a" },
           { category: "b", region: "b" }
         )
